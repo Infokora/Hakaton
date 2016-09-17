4fcc0ff8177e7a0e7fd23e50d11b997ebe89bcf1
SLot <anriy.slotvinskiy@gmail.com>
2016-09-17 16:29:53 +0300

    karkas

--- main/js/script.js
+++ main/js/script.js
@@ -1,5 +1,11 @@
 $(function(){
-
+	$('.nav li>').hover(function(){
+		$(this).css('background', '#1E90FF')
+		$(this).css('transition','background 0.6s');
+	},function(){
+		$(this).css('background', '#000')
+		$(this).css('transition','background 0.6s');
+	});
 
 
 
