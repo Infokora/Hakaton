46411639c1f2cef5b37dc905934ebc18272f5d83
RomaSlobodeniuk <romsl1991@gmail.com>
2016-09-17 19:13:50 +0300

    MVC with router - setting

--- components/Router.php
+++ components/Router.php
@@ -1,12 +1,50 @@
 <?php
 
-/**
- * Created by PhpStorm.
- * User: Администратор
- * Date: 17.09.2016
- * Time: 17:43
- */
 class Router
 {
+    private $routes;
+    public $uri;
+
+    /*
+     * include file "routes.php" from folder "config" with array inside
+     * */
+    public function __construct()
+    {
+        $routesPath = ROOT . 'config/routes.php';
+        $this->routes = include($routesPath);
+    }
+
+
+    /*
+     * return String from request field
+     * */
+    private function getURI()
+    {
+
+        if (!empty($_SERVER['REQUEST_URI'])) {
+            $this->uri = str_replace('Hakaton/', '', $_SERVER['REQUEST_URI']);
+            $this->uri = trim($this->uri, '/');
+            return $this->uri;
+        }
+    }
+
+
+    public function run()
+    {
+        $uri = $this->getURI();
+//        var_dump($this->routes);
+//        echo $uri;
+
+        foreach ($this->routes as $uriPattern => $path) {
+            if (preg_match("/$uriPattern/", $uri))
+            {
+                echo "EXIST, EXECUTING";
+            }
+//            echo $uriPattern.'<br>';
+//            echo $path.'<br>';
+        }
+
+
+    }
 
 }
--- index.php
+++ index.php
@@ -3,3 +3,11 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
+define('ROOT', __DIR__.'/');
+//echo ROOT;
+require_once (ROOT.'/components/Router.php');
+//echo '<br>';
+//echo ROOT.'/components/Router.php';
+
+$router = new Router();
+$router->run();
