eb0a783d510e602314ad52a8d30699b72aef2a7b
SLot <anriy.slotvinskiy@gmail.com>
2016-09-18 14:35:40 +0300

    main and page create news

--- main/js/script.js
+++ main/js/script.js
@@ -24,6 +24,8 @@ $(function(){
 		$(this).hide(400);
 		$('#popup').hide(400);
 	});
+
+
 	$('.form_log label').hover(function(){
 		$(this).css('box-shadow',' 5px 5px 10px rgba(0,0,0,0.7)');
 		$(this).css('transition','box-shadow 0.2s');
@@ -32,5 +34,31 @@ $(function(){
 		$(this).css('box-shadow',' none');
 		$(this).css('transition','box-shadow 0.2s');
 	});
+
+	$('.form_in label').hover(function(){
+		$(this).css('box-shadow',' 5px 5px 10px rgba(0,0,0,0.7)');
+		$(this).css('transition','box-shadow 0.2s');
+
+	}, function(){
+		$(this).css('box-shadow',' none');
+		$(this).css('transition','box-shadow 0.2s');
+	});
+
+	/*var kilcPar=$('.spusok li');
+	var polovuna=kilcPar.length/2;
+	console.log(polovuna);
+	function randNum(min, max) {
+	   return Math.floor(Math.random() * (kilcPar.length - polovuna + 1)) + polovuna;
+	};
+var a;
+	var masSpuska = [];                                      
+	for(i=0; i<kilcPar.length; i++) {
+		a=randNum();
+		kilcPar[].append('')
+	};
+	console.log(masSpuska);
+	*/
+
+
 
 });
