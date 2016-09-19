9c2754fb0c8c11c5e6eb76affe49da0df9760fa1
SLot <anriy.slotvinskiy@gmail.com>
2016-09-19 15:29:14 +0300

    доробив сторінку з жеребкуванням, виставленням оцінок, підрахунком результатів та створенням списку тих хто пройшов в фінал

--- main/js/script.js
+++ main/js/script.js
@@ -44,6 +44,15 @@ $(function(){
 		$(this).css('transition','box-shadow 0.2s');
 	});
 
+		$('.form_math label').hover(function(){
+		$(this).css('box-shadow',' 5px 5px 10px rgba(0,0,0,0.7)');
+		$(this).css('transition','box-shadow 0.2s');
+
+	}, function(){
+		$(this).css('box-shadow',' none');
+		$(this).css('transition','box-shadow 0.2s');
+	});
+
 
 
 	$('#jereb_run').on('click',function(){
@@ -76,13 +85,63 @@ $(function(){
 		kilcPar.each(function(i){
 			$(this).append(' - '+masRand[i])
 		});
-
 		$('.two_team').empty();
 
+		$(this).text('Показать результаты');
+		$('.cenu').css('display','block');
 		$('#jereb_run').off('click');
+
+			//final 10 6
+		$('#jereb_run').click(function () {
+			var participantsInfo={},
+			participantsPoints=[],
+			participantsWinnersNames=[],
+			$participants=$('div.participants'),
+			$points=$('ul.participants_points'),
+			$winnersList=$('div.the_final_6');
+
+			for (var i=0; i<$participants.children().length; i++) {
+				participantsInfo[$participants.children().eq(i).children().eq(0).html()]=$points.children().eq(i).children()[0].value;
+			}
+
+			for (var key in participantsInfo) {
+				participantsPoints.push(+participantsInfo[key])
+			}
+
+			participantsPoints.sort(function(a,b){return a-b;}).reverse()
+			console.log(participantsPoints)
+
+			addingWinnersProcess:
+			for (var i=0; i<6; i++) {
+				for (var key in participantsInfo) {
+					if (participantsPoints[i]==participantsInfo[key]) {
+						participantsWinnersNames.push(key);
+						delete participantsInfo[key];
+						if (participantsWinnersNames.length==6) {
+							break addingWinnersProcess;
+						}
+					}
+				}
+			}
+
+			console.log(participantsWinnersNames)
+
+			for (var i=0; i<$winnersList.children(0).eq(0).children().length-1; i++) {
+				$winnersList.children().eq(0).children().eq(i).html(participantsWinnersNames[i]+'. Пара набрала: '+participantsPoints[i]+' балов')
+			}
+
+			$winnersList.css("display", "block")
+
+			$('#the_final_6_close').click(function () {
+				$winnersList.css("display", "none")
+			})
+		})
 	});
 
 
+
+
+   
 
 
 });
