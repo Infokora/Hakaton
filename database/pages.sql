6e612b6dbaa2c92a39d409187e0b0597b6a411c3
RomaSlobodeniuk <romsl1991@gmail.com>
2016-09-20 17:28:18 +0300

    I've updated the users table

--- database/pages.sql
+++ database/pages.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Хост: 127.0.0.1
--- Время создания: Сен 20 2016 г., 10:20
+-- Время создания: Сен 20 2016 г., 16:27
 -- Версия сервера: 10.1.9-MariaDB
 -- Версия PHP: 5.6.15
 
@@ -42,14 +42,15 @@ CREATE TABLE `pages` (
 
 INSERT INTO `pages` (`id`, `title`, `link`, `description`, `active`, `grant`, `class`) VALUES
 (1, 'Главная', 'home', 'without_popup', 1, 0, ''),
-(8, 'Создать событие', 'addevent', 'without_popup', 1, 1, 'evant_animate'),
-(9, 'Создать новость', 'addnews', 'without_popup', 1, 1, 'news_animate'),
+(8, 'Создать событие', 'addevent', 'without_popup', 1, 4, 'evant_animate'),
+(9, 'Создать новость', 'addnews', 'without_popup', 1, 4, 'news_animate'),
 (2, 'Новости', 'news', 'without_popup', 1, 0, ''),
 (3, 'События', 'events', 'without_popup', 1, 0, ''),
 (4, 'О нас', 'about', 'without_popup', 1, 0, ''),
 (5, 'Связь с нами', '#', 'popup', 1, 0, 'connect'),
 (6, 'Вход', '#', 'popup', 1, 2, 'log_animate'),
 (7, 'Выход', 'out', 'without_popup', 1, 1, ''),
+(11, 'Профиль', 'profile', 'without_popup', 1, 1, ''),
 (10, 'Регистрация', 'home#registration_form', 'without_popup', 1, 2, '');
 
 --
@@ -70,7 +71,7 @@ ALTER TABLE `pages`
 -- AUTO_INCREMENT для таблицы `pages`
 --
 ALTER TABLE `pages`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
