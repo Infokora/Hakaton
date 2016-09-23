0de9c0a9f852b77a539679bfd3ae67e1f2f60584
SLot <anriy.slotvinskiy@gmail.com>
2016-09-23 16:02:35 +0300

    bag delete

--- main/js/script.js
+++ main/js/script.js
@@ -30,12 +30,6 @@ $(function(){
     $next.on('click',goSlide);
     $prev.on('click',backSlide);
 
-
-    $('section, span, #popup-news .buttons, #overlay').on('click', function () {
-        $('#popup-news, #overlay').toggle();
-    });
-
-
     //function2
     $('.nav li').hover(function(){
         $(this).css('background', '#1E90FF')
